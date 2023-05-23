313c1cfa454ce842a3ea8c67ff6bc3f62c3cc445
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-05-23 10:39:52 +0700

    Front-End: Home Page Clean Up

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,20 +3,14 @@ import React from "react";
 import "./App.css";
 
 import Navbar from "./layouts/NavbarAndFooter/Navbar";
-import { ExploreTopBook } from "./layouts/HomePage/ExploreTopBook";
-import { Carousel } from "./layouts/HomePage/Carousel";
-import { Heros } from "./layouts/HomePage/Heros";
-import { LibraryServices } from "./layouts/HomePage/LibraryServices";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ExploreTopBook />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
       <Footer />
     </div>
   );
