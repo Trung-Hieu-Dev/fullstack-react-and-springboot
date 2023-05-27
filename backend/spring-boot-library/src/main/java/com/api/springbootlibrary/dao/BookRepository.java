7871670c5f01e7b60644cf5fdd2c09dc7ec08787
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-05-27 08:32:09 +0700

    Back-End: Search Book By Category Api

--- backend/spring-boot-library/src/main/java/com/api/springbootlibrary/dao/BookRepository.java
+++ backend/spring-boot-library/src/main/java/com/api/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,15 @@
 package com.api.springbootlibrary.dao;
 
 import com.api.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    // search book by title
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    // search book by category
+    Page<Book> findByCategoryContaining(@RequestParam("category") String category, Pageable pageable);
 }
