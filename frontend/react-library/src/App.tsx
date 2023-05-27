eeb832a9e26699258b2bacba9e595936eed3be35
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-05-27 09:55:32 +0700

    Front-End: Adding Route using react-router-dom@5

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -6,13 +6,23 @@ import Navbar from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBookPage } from "./layouts/SearchBookPage/SearchBookPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBookPage />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBookPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
