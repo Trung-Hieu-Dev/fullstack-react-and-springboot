8cbfaad95a68e4b0ccf1194452ab6119a1b7ea73
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-05 16:49:43 +0700

    Back-End: Checkout - Current Loans Count Endpoint

--- backend/spring-boot-library/src/main/java/com/api/springbootlibrary/dao/CheckoutRepository.java
+++ backend/spring-boot-library/src/main/java/com/api/springbootlibrary/dao/CheckoutRepository.java
@@ -3,6 +3,10 @@ package com.api.springbootlibrary.dao;
 import com.api.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }
